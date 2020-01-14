2856dca879c6a50d722c6d1a44094cf4d57dd9de
Roderick Romero <rjrr507@gmail.com>
2020-01-13 22:06:00 -0500

    modificacion de la conexion a base de datos

--- models/connection.js
+++ models/connection.js
@@ -1,20 +1,44 @@
 const mongoose = require('mongoose');
-const config = require('../config/databaseConfig');
 
-let url = '';
+class Connection extends mongoose.Mongoose {
+	constructor() {
+		super();
+		this.connection;
+		this.db;
+		this.init();
+	}
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-	url = process.env.MONGO_ATLAS_URL;
-} else {
-	url = process.env.DEV_MONGO_ATLAS_URL;
+	async init(count = 0) {
+		if (count < 5) {
+			try {
+				let url = '';
+
+				if (process.env.NODE_ENV === 'PRODUCTION') {
+					url = process.env.MONGO_ATLAS_URL;
+				} else {
+					url = process.env.DEV_MONGO_ATLAS_URL;
+				}
+
+				this.connection = await this.connect(url, {
+					useNewUrlParser: true,
+					useCreateIndex: true,
+					useUnifiedTopology: true,
+				});
+				this.db = this.connection;
+			} catch (error) {
+				console.warn(error);
+				console.info('intentando conectar nuevamente...');
+				setTimeout(() => this.init((count += 1)), 1000);
+			}
+		}
+
+		if (count > 4) {
+			console.error(
+				'no se pudo establecer una conexion exitosa a la base de datos, verifque sus configracion'
+			);
+		}
+	}
 }
 
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-});
-
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
+module.exports.Mongoose = Connection;
+module.exports = new Connection();
