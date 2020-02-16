183598bc843520a614befa664d273f6b2e3aecb5
Roderick Romero <rjrr507@gmail.com>
2020-02-15 19:32:52 -0500

    cambios

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const { Usuario, Imagenes, Producto } = require('../models');
 module.exports.getProfile = async (req, res, next) => {
 	try {
 		console.log('ruta de Perfil de Usuario');
-		const user = await Usuario.findById(req.user.id, '-roll -password');
+		const user = await Usuario.findById(req.user.id, '-__v -roll -password');
 		user.foto = await Imagenes.findOne({ usuario: user.id });
 
 		res.status(200).json({ success: true, data: user });
--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -2,6 +2,13 @@ const { extractToken, token } = require('../utils');
 
 const isAuthenticated = async (req, res, next) => {
 	try {
+		const newError = new Error();
+		if (!req.headers.authorization) {
+			newError.name = 'Sin Accesso';
+			newError.message = 'No Tienes Acceso, token missing';
+			throw newError;
+		}
+
 		const tmpToken = extractToken(req.headers.authorization);
 		const verifyToken = await token.verify(tmpToken);
 
@@ -10,7 +17,12 @@ const isAuthenticated = async (req, res, next) => {
 		next();
 	} catch (error) {
 		console.log('catch de is authenticated', error);
-		next(error);
+		//next(error);
+		res.status(401).json({
+			success: false,
+			title: 'Sin Autorizacion',
+			message: error.message,
+		});
 	}
 };
 
